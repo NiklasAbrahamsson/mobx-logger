dfdd71a54ecaa498e4552a90693ccae9a4fe5cfd
NiklasAbrahamsson <Niklas.abrahamsson@me.com>
2021-01-15 14:14:26 +0100

    Changed color for the logger to work in darkmode

--- src/log.js
+++ src/log.js
@@ -1,125 +1,166 @@
-import {padStart, now} from './utils';
+'use strict';
 
-const style = (color, bold = true) => {
-    return `color:${color};font-weight:${bold ? '600' : '300'};font-size:11px`;
+Object.defineProperty(exports, '__esModule', {
+  value: true,
+});
+
+var _utils = require('./utils');
+
+var style = function style(color) {
+  var bold =
+    arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : true;
+
+  return (
+    'color:' +
+    color +
+    ';font-weight:' +
+    (bold ? '600' : '300') +
+    ';font-size:11px'
+  );
 };
 
-const defaultLoggerConfig = {
-    disable: false,
-    functions: {}
+var defaultLoggerConfig = {
+  disable: false,
+  functions: {},
 };
 
-const getLoggerConfig = (ev) => {
-    if (ev.object == null) {
-        return defaultLoggerConfig;
-    }
-    const loggerConfig = ev.object.constructor.mobxLoggerConfig;
-    return loggerConfig == null
-        ? defaultLoggerConfig
-        : loggerConfig;
+var getLoggerConfig = function getLoggerConfig(ev) {
+  if (ev.object == null) {
+    return defaultLoggerConfig;
+  }
+  var loggerConfig = ev.object.constructor.mobxLoggerConfig;
+  return loggerConfig == null ? defaultLoggerConfig : loggerConfig;
 };
 
-const isLoggingEnabled = (ev) => {
-    if (ev.object == null) {
-        return false;
-    }
-    const loggerConfig = getLoggerConfig(ev);
-    if (loggerConfig == null) {
-        return true;
-    }
-    const enabled = loggerConfig.enabled === true || loggerConfig.enabled == null;
-    if (loggerConfig.methods == null) {
-        return enabled;
-    }
-    const propertyName = getPropName(ev);
-    const methodLoggerConfig = loggerConfig.methods[propertyName];
-    if (methodLoggerConfig == null) {
-        return enabled;
-    }
-    if (methodLoggerConfig === true) {
-        return true;
-    }
-    if (methodLoggerConfig === false) {
-        return false;
-    }
-    return methodLoggerConfig.enabled !== false;
+var isLoggingEnabled = function isLoggingEnabled(ev) {
+  if (ev.object == null) {
+    return false;
+  }
+  var loggerConfig = getLoggerConfig(ev);
+  if (loggerConfig == null) {
+    return true;
+  }
+  var enabled = loggerConfig.enabled === true || loggerConfig.enabled == null;
+  if (loggerConfig.methods == null) {
+    return enabled;
+  }
+  var propertyName = getPropName(ev);
+  var methodLoggerConfig = loggerConfig.methods[propertyName];
+  if (methodLoggerConfig == null) {
+    return enabled;
+  }
+  if (methodLoggerConfig === true) {
+    return true;
+  }
+  if (methodLoggerConfig === false) {
+    return false;
+  }
+  return methodLoggerConfig.enabled !== false;
 };
 
-const logAction = (ev) => {
-    if (!isLoggingEnabled(ev)) {
-        return;
-    }
+var logAction = function logAction(ev) {
+  if (!isLoggingEnabled(ev)) {
+    return;
+  }
 
-    console.groupCollapsed('%c%s  %s  %s.%s()', style('#000'), now(), padStart('ACTION', 8), ev.object.name || ev.object, ev.name);
-    console.log('%cFunction %o', style('#777'), ev.fn);
-    console.log('%cArguments %o', style('#777'), ev.arguments);
-    console.log('%cTarget %o', style('#777'), ev.object);
-    console.log('%cEvent %o', style('#777'), ev);
-    console.groupEnd();
+  console.groupCollapsed(
+    '%c%s  %s  %s.%s()',
+    style('#fe424d'),
+    (0, _utils.now)(),
+    (0, _utils.padStart)('ACTION', 8),
+    ev.object.name || ev.object,
+    ev.name
+  );
+  console.log('%cFunction %o', style('#fe424d'), ev.fn);
+  console.log('%cArguments %o', style('#fe424d'), ev.arguments);
+  console.log('%cTarget %o', style('#fe424d'), ev.object);
+  console.log('%cEvent %o', style('#fe424d'), ev);
+  console.groupEnd();
 };
 
-const logReaction = (ev) => {
-    const name = ev.name.replace('#null', '');
-    console.groupCollapsed('%c%s  %s  %s', style('#9E9E9E'), now(), padStart('REACTION', 8), name);
+var logReaction = function logReaction(ev) {
+  var name = ev.name.replace('#null', '');
+  console.groupCollapsed(
+    '%c%s  %s  %s',
+    style('#fe424d'),
+    (0, _utils.now)(),
+    (0, _utils.padStart)('REACTION', 8),
+    name
+  );
 
-    const observables = ev.observing || [];
-    const names = observables.map(it => it.name);
-    if (names.length > 0) {
-        console.log('%cObserving %o', style('#777'), names);
-    }
+  var observables = ev.observing || [];
+  var names = observables.map(function (it) {
+    return it.name;
+  });
+  if (names.length > 0) {
+    console.log('%cObserving %o', style('#fe424d'), names);
+  }
 
-    console.log('%cEvent %o', style('#777'), ev);
-    console.groupEnd();
+  console.log('%cEvent %o', style('#fe424d'), ev);
+  console.groupEnd();
 };
 
-const logTransaction = (ev) => {
-    console.groupCollapsed('%c%s  %s  %s', style('#7B7B7B'), now(), padStart('TX', 8), ev.name);
-    console.log('%cEvent %o', style('#777'), ev);
-    console.groupEnd();
+var logTransaction = function logTransaction(ev) {
+  console.groupCollapsed(
+    '%c%s  %s  %s',
+    style('#7B7B7B'),
+    (0, _utils.now)(),
+    (0, _utils.padStart)('TX', 8),
+    ev.name
+  );
+  console.log('%cEvent %o', style('#fe424d'), ev);
+  console.groupEnd();
 };
 
-const logCompute = (ev) => {
-    if (!isLoggingEnabled(ev)) {
-        return;
-    }
-    const name = ev.object;
-    let propName = getPropName(ev);
-    if (propName) {
-        propName = `.${propName}`;
-    }
-    console.groupCollapsed('%c%s  %s  %s%s', style('#9E9E9E'), now(), padStart('COMPUTE', 8), name, propName);
-    console.log('%cEvent %o', style('#777'), ev);
-    console.groupEnd();
+var logCompute = function logCompute(ev) {
+  if (!isLoggingEnabled(ev)) {
+    return;
+  }
+  var name = ev.object;
+  var propName = getPropName(ev);
+  if (propName) {
+    propName = '.' + propName;
+  }
+  console.groupCollapsed(
+    '%c%s  %s  %s%s',
+    style('#9E9E9E'),
+    (0, _utils.now)(),
+    (0, _utils.padStart)('COMPUTE', 8),
+    name,
+    propName
+  );
+  console.log('%cEvent %o', style('#fe424d'), ev);
+  console.groupEnd();
 };
 
-const getPropName = (ev) => {
-    if (ev.name != null) {
-        return ev.name;
-    }
-    return Object
-            .keys(ev.object.$mobx.values)
-            .filter(
-                key => ev.object.$mobx.values[key].derivation === ev.fn
-            )[0] || '';
+var getPropName = function getPropName(ev) {
+  if (ev.name != null) {
+    return ev.name;
+  }
+  return (
+    Object.keys(ev.object.$mobx.values).filter(function (key) {
+      return ev.object.$mobx.values[key].derivation === ev.fn;
+    })[0] || ''
+  );
 };
 
-export default (ev, options) => {
-    if (options[ev.type] !== true) {
-        return;
-    }
+exports.default = function (ev, options) {
+  if (options[ev.type] !== true) {
+    return;
+  }
 
-    switch (ev.type) {
-        case 'action':
-            logAction(ev);
-            return;
-        case 'reaction':
-            logReaction(ev);
-            return;
-        case 'transaction':
-            logTransaction(ev);
-            return;
-        case 'compute':
-            logCompute(ev);
-            return;
-    }
-}
+  switch (ev.type) {
+    case 'action':
+      logAction(ev);
+      return;
+    case 'reaction':
+      logReaction(ev);
+      return;
+    case 'transaction':
+      logTransaction(ev);
+      return;
+    case 'compute':
+      logCompute(ev);
+      return;
+  }
+};
